34c6eacf7495c74c73fbdce09621e7e8776ac164
Shungo Nakanishi <snakanishi@ShungonoMacBook-Pro.local>
2021-03-03 19:33:04 +0900

    add system.properties

--- src/main/java/com/example/mydemo/web/controller/MainController.java
+++ src/main/java/com/example/mydemo/web/controller/MainController.java
@@ -150,7 +150,8 @@ public class MainController {
             var client = new RestTemplate(new SimpleClientHttpRequestFactory());
             var headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-            var uri = new URI(String.format("http://%s/balance", bcsProperties.getHostUri()));
+            var uri = new URI(String.format("https://%s/balance", System.getenv("BLOCKCHAIN_URL")));
+            // var uri = new URI(String.format("http://%s/balance", bcsProperties.getHostUri()));
             var queryUri = UriComponentsBuilder
                 .fromUri(uri)
                 .queryParam("address", address)
@@ -177,7 +178,8 @@ public class MainController {
     public ResponseEntity<String> requestTransaction(TransactionRequest transactionReq) {
         try {
             var client = new RestTemplate(new SimpleClientHttpRequestFactory());
-            var uri = new URI(String.format("http://%s/transaction", bcsProperties.getHostUri()));
+            var uri = new URI(String.format("https://%s/transaction", System.getenv("BLOCKCHAIN_URL")));
+            // var uri = new URI(String.format("http://%s/transaction", bcsProperties.getHostUri()));
             var request = RequestEntity
                 .post(uri)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -202,7 +204,8 @@ public class MainController {
     public ResponseEntity<String> requestPurchase(PurchaseRequest purchaseReq) {
         try {
             var client = new RestTemplate(new SimpleClientHttpRequestFactory());
-            var uri = new URI(String.format("http://%s/purchase", bcsProperties.getHostUri()));
+            var uri = new URI(String.format("https://%s/purchase", System.getenv("BLOCKCHAIN_URL")));
+            // var uri = new URI(String.format("http://%s/purchase", bcsProperties.getHostUri()));
             var request = RequestEntity
                 .post(uri)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -227,7 +230,8 @@ public class MainController {
     public ResponseEntity<String> requestInfo() {
         try {
             var client = new RestTemplate(new SimpleClientHttpRequestFactory());
-            var uri = new URI(String.format("http://%s/info", bcsProperties.getHostUri()));
+            var uri = new URI(String.format("https://%s/info", System.getenv("BLOCKCHAIN_URL")));
+            // var uri = new URI(String.format("http://%s/info", bcsProperties.getHostUri()));
             var req = RequestEntity.get(uri).build();
             return client.exchange(req, String.class);
 
