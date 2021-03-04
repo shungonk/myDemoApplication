c87e7a669d90df34887757cadf9612d9397205cb
Shungo Nakanishi <snakanishi@ShungonoMacBook-Pro.local>
2021-03-04 22:21:56 +0900

    fix:

--- src/main/java/com/example/mydemo/web/controller/MainController.java
+++ src/main/java/com/example/mydemo/web/controller/MainController.java
@@ -150,7 +150,7 @@ public class MainController {
             var headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
             var uri = UriComponentsBuilder
-                .fromUri(bcsProps.getUrl())
+                .fromUri(bcsProps.getUri())
                 .path("/balance")
                 .queryParam("address", address)
                 .build().encode().toUri();
@@ -179,7 +179,7 @@ public class MainController {
             var headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             var uri = UriComponentsBuilder
-                .fromUri(bcsProps.getUrl())
+                .fromUri(bcsProps.getUri())
                 .path("/transaction")
                 .build().encode().toUri();
             var req = new RequestEntity<>(transactionReq.toJson(), headers, HttpMethod.POST, uri);
@@ -206,7 +206,7 @@ public class MainController {
             var headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             var uri = UriComponentsBuilder
-                .fromUri(bcsProps.getUrl())
+                .fromUri(bcsProps.getUri())
                 .path("/purchase")
                 .build().encode().toUri();
             var req = new RequestEntity<>(purchaseReq.toJson(), headers, HttpMethod.POST, uri);
@@ -231,7 +231,7 @@ public class MainController {
         try {
             var client = new RestTemplate(new SimpleClientHttpRequestFactory());
             var uri = UriComponentsBuilder
-                .fromUri(bcsProps.getUrl())
+                .fromUri(bcsProps.getUri())
                 .path("/info")
                 .build().encode().toUri();
             var req = new RequestEntity<>(HttpMethod.GET, uri);
