1900375dd313551d7ce16ab764d308cc7d339ce3
Shungo Nakanishi <snakanishi@ShungonoMacBook-Pro.local>
2021-03-04 21:33:54 +0900

    changet to use PostgreSql at loal

--- ddl/create_table_user.sql
+++
@@ -1,8 +0,0 @@
--- MySQL
-CREATE TABLE users (
-  username VARCHAR(50) NOT NULL
-  , password VARCHAR(100) NOT NULL
-  , enabled TINYINT NOT NULL
-  , active TINYINT NOT NULL
-  , CONSTRAINT pk_users PRIMARY KEY (username)
-);
