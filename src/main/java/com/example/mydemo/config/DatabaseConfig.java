b77e553e0ffd7e2130313688fcc458ba3117cbdf
Shungo Nakanishi <snakanishi@ShungonoMacBook-Pro.local>
2021-03-04 23:31:12 +0900

    fix

--- src/main/java/com/example/mydemo/config/DatabaseConfig.java
+++ src/main/java/com/example/mydemo/config/DatabaseConfig.java
@@ -1,18 +1,26 @@
 package com.example.mydemo.config;
 
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// local - need to comment out this class
 // Heroku
 @Configuration
 public class DatabaseConfig {
 
-    // @Value("${spring.datasource.url}")
-    // private String dbUrl;
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
-    // @Bean
-    // public DataSource dataSource() {
-    //     HikariConfig config = new HikariConfig();
-    //     config.setJdbcUrl(dbUrl);
-    //     return new HikariDataSource(config);
-    // }
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
 }
