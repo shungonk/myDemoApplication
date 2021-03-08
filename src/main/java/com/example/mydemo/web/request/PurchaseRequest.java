a0fe4b34d339cf1cd896c181db4171ee99178348
Shungo Nakanishi <snakanishi@ShungonoMacBook-Pro.local>
2021-03-08 12:27:08 +0900

    fix

--- src/main/java/com/example/mydemo/web/request/PurchaseRequest.java
+++ src/main/java/com/example/mydemo/web/request/PurchaseRequest.java
@@ -5,34 +5,72 @@ import java.security.GeneralSecurityException;
 
 import com.example.mydemo.util.SecurityUtil;
 
+/**
+ * 通貨購入のリクエストクラス
+ * 
+ * <p>{@link SignatureCertifier}抽象クラスを継承します。
+ * 
+ * @author  S.Nakanishi
+ * @version 1.0
+ * @see     SignatureCertifier
+ */
 public class PurchaseRequest extends SignatureCertifier {
 
     private String address;
     private BigDecimal amount;
     private long timestamp;
 
+    /**
+     * コンストラクタ
+     * 
+     * @param address
+     *        購入者アドレス
+     * @param amount
+     *        購入金額
+     * @param timestamp
+     *        時刻（エポック秒）
+     */
     public PurchaseRequest(String address, BigDecimal amount, long timestamp) {
         this.address = address;
         this.amount = amount;
         this.timestamp = timestamp;
     }
 
+    /**
+     * コンストラクタで設定した購入者アドレスを取得します。
+     * 
+     * @return {@link #address}
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * コンストラクタで設定した購入金額を取得します。
+     * 
+     * @return {@link #amount}
+     */
     public BigDecimal getAmount() {
         return amount;
     }
 
+    /**
+     * コンストラクタで設定した時刻（エポック秒）を取得します。
+     * 
+     * @return {@link #timestamp}
+     */
     public long getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * コンストラクタで設定した購入者アドレス、購入金額、時刻（エポック秒）を連結した
+     * 文字列からバイト配列を取得します。
+     */
     @Override
     public byte[] getData() {
-        // signature will be unique because data contain timestamp
-        String data = address + amount.toPlainString() + Long.toString(timestamp);
+        String data = address
+             + amount.toPlainString() + Long.toString(timestamp);
         return data.getBytes();
     }
 
@@ -40,7 +78,7 @@ public class PurchaseRequest extends SignatureCertifier {
     public boolean validateFields() {
         if (publicKey == null || publicKey.isBlank() ||
             address == null || address.isBlank() ||
-            amount == null || amount.equals(BigDecimal.ZERO) ||
+            amount == null ||
             signature == null || signature.isBlank() ||
             timestamp == 0) {
             return false;
@@ -48,17 +86,28 @@ public class PurchaseRequest extends SignatureCertifier {
         return true;
     }
 
+    /**
+     * 購入金額が正の値であるかどうかを検証します。
+     * 
+     * @return {@code true} 購入金額が０より大きい場合
+     */
     public boolean validateAmount() {
         return amount.compareTo(BigDecimal.ZERO) > 0;
     }
-    
-    public final boolean verifyAddress() {
+
+    /**
+     * 購入者アドレスを検証します。
+     * 
+     * @return {@code true} 送金元アドレスがパブリックキー
+     * （{@link SignatureCertifier#publicKey}）から生成されたものである場合
+     * @see    SecurityUtil#verifyAddressByPublicKey(String,String)
+     */
+    public boolean verifyAddress() {
         try {
             return SecurityUtil.verifyAddressByPublicKey(address, publicKey);
         } catch (GeneralSecurityException e) {
-            // LogWriter.warning("Falied to verify address by public key");
+            e.printStackTrace();
             return false;
         }
     }
 }
-
--- src/main/java/com/example/mydemo/web/request/SignatureCertifier.java
+++ src/main/java/com/example/mydemo/web/request/SignatureCertifier.java
@@ -1,52 +1,129 @@
 package com.example.mydemo.web.request;
 
 import java.security.GeneralSecurityException;
+import java.security.PublicKey;
+import java.security.Signature;
 
 import com.example.mydemo.util.SecurityUtil;
 import com.example.mydemo.util.StringUtil;
 
-
+/**
+ * シグネチャ認証を行う抽象クラス
+ * 
+ * @author  S.Nakanishi
+ * @version 1.0
+ */
 public abstract class SignatureCertifier {
 
-    protected String publicKey; // will be set in signate method
-    protected String signature; // will be set in signate method
+    /**
+     * {@link PublicKey}のBase64エンコードされた文字列です。シグネチャ認証に使用
+     * します。{@link #signate(String, String)}実行時に値が格納されます。
+     */
+    protected String publicKey;
+
+    /**
+     * {@link Signature}のBase64エンコードされた文字列です。
+     * {@link #signate(String, String)}実行時に値が格納されます。
+     */
+    protected String signature;
 
     protected SignatureCertifier() {}
 
+    /**
+     * シグネチャ認証に使用するBase64表現のパブリックキー文字列を取得します。
+     * 
+     * @return {@link #publicKey}
+     */
     public String getPublicKey() {
         return publicKey;
     }
 
+    /**
+     * Base64表現のシグネチャ文字列を取得します。
+     * 
+     * @return {@link #signature}
+     */
     public String getSignature() {
         return signature;
     }
 
+    /**
+     * シグネチャ生成に使用するバイト配列データを取得します。
+     * 
+     * @return バイト配列データ
+     */
     public abstract byte[] getData();
 
+    /**
+     * 全フィールドに値が設定されているかどうかを検証します。
+     * 
+     * @return {@code true} 全て条件を満たしている場合
+     */
     public abstract boolean validateFields();
 
+    /**
+     * シグネチャを生成します。
+     * 
+     * シグネチャ生成に使用するバイト配列データは{@link #getData()}から取得します。生成
+     * されたシグネチャと{@code publicKey}パラメータは、{@link #signature}, 
+     * {@link #publicKey}フィールドに格納されます（{@link #publicKey}はシグネチャ認証
+     * に使用します）。
+     * 
+     * @param privateKey 
+     *        シグネチャ生成に使用するBase64エンコード表現したプライベートキー文字列。
+     * @param publicKey
+     *        シグネチャ認証に使用するBase64エンコード表現したパブリックキー文字列。
+     * @see   #getData()
+     * @see   #verifySignature()
+     * @see   SecurityUtil#createEcdsaSign(String, byte[])
+     */
     public final void signate(String privateKey, String publicKey) {
         try {
             this.publicKey = publicKey;
             this.signature = SecurityUtil.createEcdsaSign(privateKey, getData());
         } catch (GeneralSecurityException e) {
-            // LogWriter.warning("Failed to create ECDSA signature");
+            e.printStackTrace();
         }
     }
 
+    /**
+     * シグネチャを認証します。
+     * 
+     * 認証には{@link #publicKey}, {@link #getData()}, {@link #signature}を使用します。
+     * 
+     * @return {@code true} シグネチャが有効である場合
+     * 
+     * @see   #getData()
+     * @see   #signate(String,String)
+     * @see   SecurityUtil#verifyEcdsaSign(String, byte[], String)
+     */
     public final boolean verifySignature() {
         try {
             return SecurityUtil.verifyEcdsaSign(publicKey, getData(), signature);
         } catch (GeneralSecurityException e) {
-            // LogWriter.warning("Failed to verity ECDSA signature");
+            e.printStackTrace();
             return false;
         }
     }
 
+    /**
+     * インスタンスのJsonを取得します。
+     * 
+     * @return Json文字列
+     * 
+     * @see    StringUtil#toJson(Object)
+     */
     public String toJson() {
         return StringUtil.toJson(this);
     }
 
+    /**
+     * インスタンスの整形されたJsonを取得します。
+     * 
+     * @return 整形されたJson文字列
+     * 
+     * @see    StringUtil#toJsonPrettyPrinting(Object)
+     */
     public String toJsonPrettyPrinting() {
         return StringUtil.toJsonPrettyPrinting(this);
     }
